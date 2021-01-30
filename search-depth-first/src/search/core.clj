a5cdd3ae6e395cb8416a01a0bb66616e56ce41dc
James Muguira <muguira.james@gmail.com>
2021-01-29 23:08:51 -0500

    Working core.clj
    Now uses recur.  Follows the recipe in the book.  Much nicer implementation
    
    d-first uses a que and a save function. The save-function saves children to the front (depth-first search) or the end (breath first search) of the queue

--- search-depth-first/src/search/core.clj
+++ search-depth-first/src/search/core.clj
@@ -16,77 +16,98 @@
    })
 
 
-;;
-;; lst is the working list of nodes I've visited
-(def lst ())
-
-;;
-;; q is the path through the network
-(def q [])
-
-;;
-;; add item i onto lst
-(defn enq [i]
-  (def lst (conj lst i)))
-
-;;
-;; save item i on to q (i.e. the path through the network
-(defn save-visited [i]
-  (def q (conj q i)))
-
-;;
-;; remove first node from lst
-(defn deq []
-  (def lst (pop lst)))
-
-;;
-;; remove all nodes from lst
-(defn drain-lst []
-  (while (not-empty lst)
-    (def lst (pop lst))))
-
-;;
-;; remove all nodes from q
-(defn drain-q []
-  (while (not-empty q)
-    (def q (pop q))))
-
-;;
-;; for this node, add children to lst
-(defn addchildren [n]
-  (let [left ((net n) :l)
-        right ((net n) :r)]
-    (do
-      (println left right)
-      (if-not (nil? left)
-        (enq left)
-        (println "left is nil"))
-      (if-not (nil? right)
-        (enq right)
-        (println "right is nil"))
-      )))
-        
-;;
-(defn depth-first []
-  "search the net using depth-first"
-  (while (not-empty lst)
-    (let [node (peek lst)]
-      (do
-        (println (str "node-->  " node " lst-> " lst))
-        (save-visited node)
-        ; if current node is our destination: print some info, drain the queue and stop
-        (if (= node 'g)
-          (do
-            (println (str  "found goal: final path-> " q))
-            (drain-lst))
-          (do
-            (deq)
-            (addchildren node))))
-
-      )))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(def que (java.util.LinkedList.))
+(def visit (java.util.LinkedList.))
+
+;;
+;; place the item on the front of the list
+(defn enque [n]
+  (.addFirst que n))
+
+(defn enque-b [n]
+  (.add que n))
+;;
+;; pop 1 item off the list
+(defn decque  []
+  (.remove que))
+
+;;
+;; clear the list
+(defn drain-q []
+  (.clear que))
+
+;;
+;; clear the list
+(defn drain-v []
+  (.clear visit))
+
+(defn drain-qs []
+  (do
+    (drain-q)
+    (drain-v)))
+;;
+;; remember where i've been
+(defn been-here [n]
+  (do
+    ;; (prn (str "visited= " n))
+    (if (not (.contains visit n))
+      (.add visit n)
+      )))
+
+;;
+;; remember the children
+(defn save-children [n]
+  (let [left ((net n) :l)
+        right ((net n) :r)]
+    (do
+      (if (and (not (.contains que left)) (not (= left nil)))
+        ;; save items on the front of the list
+        (enque left))
+      (if (and (not (.contains que right)) (not (= right nil)))
+        ;; save items on the front of the list
+        (enque right)))))
+
+;;
+;; generate a que that forces breath first search
+(defn save-b-children [n]
+  (let [left ((net n) :l)
+        right ((net n) :r)]
+    (do
+      (if (and (not (.contains que left)) (not (= left nil)))
+        ;; save items on the end of the list
+        (enque-b left))
+      (if (and (not (.contains que right)) (not (= right nil)))
+        ;; save items on the end of the list
+        (enque-b right))
+      )))
+;;
+;; depth first search
+;;
+;; usage: (d-first que save-children)
+(defn d-first [q save-fn]
+  (let [node (first q)]
+    (if (= node 'g)
+      ;; true found our goal
+      (do
+        (println (str "found my goal -> " node " visit m = " visit))
+        'g)
+      ;; false: continue searching
+      (do
+        
+        (println (str "moving forward " node " -> " que))
+        (decque)
+        (been-here node)
+        (save-fn node)
+        ;; (println (str "checking-> " que))
+        (recur  que save-fn)))))
+
+
+
