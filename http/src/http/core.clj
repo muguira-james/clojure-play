1ddfd3c145623755a391285d18d7aacabcc1a502
muguira-james <muguira.james@gmail.com>
2022-06-10 12:13:46 -0400

    add cors support

--- http/src/http/core.clj
+++ http/src/http/core.clj
@@ -22,14 +22,11 @@
   (do
     (pp/pprint req)
     (let [p (trawl/fetch (:page (:params req)))
-          l (:links p)
-          ]
+          l (:links p)]
       {
        :status 200
        :headers {"Content-Type" "application/json" }
        :body (->
-              (pp/pprint req)
-                                        ; (pp/pprint (type p))
               (str " { \"links\" : " (str (json/write-str l)) " } ")
           )
        }
@@ -55,13 +52,21 @@
   (GET "/scrape" [] scrape-web-page)
   (route/not-found "Error, page not found!"))
 
+(def app
+  (->
+   app-routes
+   (wrap-defaults api-defaults)
+   (wrap-cors
+    :access-control-allow-origin [#".*"]
+    :access-control-allow-methods [:get :post])
+   ))
 
 (defn -main
   "This is our main entry point"
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     ;; Run the server with Ring.defaults middleware
-    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    (server/run-server app {:port port})
     ;; Run the server without ring defaults
     ;;(server/run-server #'app-routes {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
